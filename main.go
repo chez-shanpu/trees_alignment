d3ecd21ea597e93ee9be2e0b0a79be72a313f00d
chez-shanpu <cheztomo513@gmail.com>
2019-11-18 17:27:14 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -21,8 +21,10 @@ func main() {
 	for key := range treeFilePaths {
 		trees[key] = readTreeJson(treeFilePaths[key])
 	}
+	//TODO なぜか同じリポジトリ同士で比較するとNaNが帰ってくる
+	//TODO それ以外だとセグメンテーションフォールトで死ぬ
 	dist := layerAlignmentDistanceTotal(trees[0].RootNode, trees[1].RootNode)
-	fmt.Printf("Alignment distance between %s and %s is %v",
+	fmt.Printf("Alignment distance between %s and %s is %f",
 		trees[0].RepositoryName, trees[1].RepositoryName, dist)
 }
 
@@ -31,7 +33,7 @@ func readTreeJson(filePath string) (tree *model.NodeInfo) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = json.Unmarshal(content, tree)
+	err = json.Unmarshal(content, &tree)
 	if err != nil {
 		log.Fatal(err)
 	}
